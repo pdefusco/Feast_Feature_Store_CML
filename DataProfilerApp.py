23a40eaf993966076a8034ea1d7f0545cf37b316
Paul de Fusco <pauldefusco@gmail.com>
2020-10-27 05:49:52 +0000

    creating profiler dashboard

---
+++ DataProfilerApp.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def login():
+    return render_template("/home/cdsw/Efficient_Model_Development_CML/example.html")
+  
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=int(os.environ['CDSW_READONLY_PORT']))
