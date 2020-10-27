0130ef445937191ab5ec88fb4d3f08be113b2854
Paul de Fusco <pauldefusco@cloudera.com>
2020-10-27 16:14:16 +0000

    updates to app

--- DataProfilerApp.py
+++ DataProfilerApp.py
@@ -7,4 +7,5 @@ def login():
     return render_template("/home/cdsw/Efficient_Model_Development_CML/example.html")
   
 if __name__ == '__main__':
+    app.debug = True
     app.run(host='127.0.0.1', port=int(os.environ['CDSW_READONLY_PORT']))
